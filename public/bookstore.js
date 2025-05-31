1a294f2f242c1bfc85cbde26689cbde930c2b10b
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-31 22:15:50 +0430

    l

--- public/bookstore.js
+++ public/bookstore.js
@@ -1,11 +1,11 @@
 const hero = document.getElementById("heroDiv");
 const images = [
-  "url(../public/img/slide1.jpg)",
-  "url(../public/img/slide2.jpg)",
-  "url(../public/img/slide3.jpg)",
-  "url(../public/img/slide4.jpg)",
-  "url(../public/img/slide5.jpg)",
-  "url(../public/img/slide6.jpg)",
+  "url(../imgse/slide1.jpg)",
+  "url(../imgse/slide2.jpg)",
+  "url(../imgse/slide3.webp)",
+  "url(../imgse/slide4.jpg)",
+  "url(../imgse/slide5.jpg)",
+  "url(../imgse/slide7.jpg)",
 ];
 let index = 0;
 setInterval(() => {
@@ -17,6 +17,7 @@ setInterval(() => {
 let indexs = 0;
 const text = "Don't Forget this : new book , new world!";
 const h1 = document.createElement("h1");
+h1.classList.add("text-3xl");
 hero.append(h1);
 setInterval(() => {
   h1.textContent += text[indexs];
@@ -25,7 +26,7 @@ setInterval(() => {
     indexs = 0;
     h1.textContent = "";
   }
-}, 2000);
+}, 1000);
 
 // setInterval(() => {
 //   const h1 = Document.createElement("h1");
