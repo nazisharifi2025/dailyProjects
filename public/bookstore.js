9cd1c222668bfaeb9736cbabb592f3edf09cd364
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-31 21:33:37 +0430

    first commit

---
+++ public/bookstore.js
@@ -0,0 +1,29 @@
+const hero = document.getElementById("heroDiv");
+const images = [
+  "url(../public/img/slide1.jpg)",
+  "url(../public/img/slide2.jpg)",
+  "url(../public/img/slide3.jpg)",
+  "url(../public/img/slide4.jpg)",
+  "url(../public/img/slide5.jpg)",
+  "url(../public/img/slide6.jpg)",
+];
+let index = 0;
+setInterval(() => {
+  hero.style.backgroundImage = images[index];
+  index = (index + 1) % images.length;
+}, 2000);
+
+// hero texts
+
+const text = "Don't Forget this : new book , new world!";
+
+const h1 = Document.createElement("h1");
+hero.appendChild(h1);
+setInterval(() => {
+  h1.textContent += text[index];
+  index++;
+  if (index > text.length) {
+    index = 0;
+    h1.textContent = "";
+  }
+}, 2000);
