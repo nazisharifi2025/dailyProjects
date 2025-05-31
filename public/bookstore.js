7b5ff09515b9b638d3b187f31f21dd8848633eb0
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-31 21:41:30 +0430

    l

--- public/bookstore.js
+++ public/bookstore.js
@@ -16,14 +16,18 @@ setInterval(() => {
 // hero texts
 
 const text = "Don't Forget this : new book , new world!";
+const h1 = document.createElement("h1");
+hero.append(h1);
+setInterval(() => {}, 2000);
 
-const h1 = Document.createElement("h1");
-hero.appendChild(h1);
-setInterval(() => {
-  h1.textContent += text[index];
-  index++;
-  if (index > text.length) {
-    index = 0;
-    h1.textContent = "";
-  }
-}, 2000);
+// setInterval(() => {
+//   const h1 = Document.createElement("h1");
+//   hero.appendChild(h1);
+//   h1
+//   h1.textContent += text[index];
+//   index++;
+//   if (index > text.length) {
+//     index = 0;
+//     h1.textContent = "";
+//   }
+// }, 5000);
