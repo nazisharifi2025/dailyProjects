aaef0179143b1dab81024bf1049ca19c53d59ecd
nazisharifi2025 <banonazisharifi@gmail.com>
2025-05-31 21:56:28 +0430

    l

--- public/bookstore.js
+++ public/bookstore.js
@@ -14,15 +14,15 @@ setInterval(() => {
 }, 2000);
 
 // hero texts
-
+let indexs = 0;
 const text = "Don't Forget this : new book , new world!";
 const h1 = document.createElement("h1");
 hero.append(h1);
 setInterval(() => {
-  h1.textContent += text[index];
-  index++;
-  if (index > text.length) {
-    index = 0;
+  h1.textContent += text[indexs];
+  indexs++;
+  if (indexs > text.length) {
+    indexs = 0;
     h1.textContent = "";
   }
 }, 2000);
