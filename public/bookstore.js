cb8bfdf3a906b453107d8d28a5b620d1b494dad1
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-02 21:28:22 +0430

    l

--- public/bookstore.js
+++ public/bookstore.js
@@ -62,3 +62,83 @@ const observer = new IntersectionObserver(
   }
 );
 counters.forEach((counter) => observer.observe(counter));
+
+// class of cart
+class Product {
+  imgUrl;
+  bookName;
+  author;
+  constructor(imgUrl, bookName, author) {
+    this.imgUrl = imgUrl;
+    this.bookName = bookName;
+    this.author = author;
+  }
+}
+//  book list array
+// const Productlist = {
+//   bookList: [
+//     new Product(
+//       "../public/img/7 habit.jpg",
+//       "7 habits of affective people",
+//       "stefan Covey"
+//     ),
+//     new Product("../public/img/5sec.jpg", "The 5 second rule", "Mell rabins"),
+//     new Product(
+//       "../public/img/sleep.jpg",
+//       "The heavy sleep",
+//       "Remond Chandler"
+//     ),
+//     new Product(
+//       "../public/img/salar.jpg",
+//       "Salar magas ha",
+//       "Willliam golding"
+//     ),
+//     new Product("../public/img/hope.jpg", "The big hopes", "Charels Dicines"),
+//     new Product("../public/img/hall.jpg", "The hall of wolf", "Hellari mintal"),
+//     new Product("../public/img/fr.jpg", "Frankishtain", "Marry shelley"),
+//     new Product("../public/img/bad.jpg", "Badbadakbaz", "Khallid Hussaini"),
+//     new Product(
+//       "../public/img/bland.jpg",
+//       "The Windful hills",
+//       "Emilly bronte"
+//     ),
+//   ],
+//   showSingleBook() {
+//     const divParent = document.getElementById("parent");
+//     this.bookList.forEach((book) => {
+//       const div = document.createElement("div");
+//       div.classList.add("cart");
+//       div.innerHTML = `<img src="${book.imgUrl}" class="h-full ">
+//       <div class="div-text">
+//       <h1>${book.bookName}</h1>
+//       <h2>${book.author}</h2>
+//       <button class="btn-add">Add to cart</button>
+//       </div>
+//       `;
+//       divParent.append(div);
+//     });
+//   },
+// };
+
+// Productlist.showSingleBook();
+
+// // animation of carts
+// const oBserver = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("animate-fade-up-show");
+//       } else {
+//         entry.target.classList.remove("animate-fade-up-show");
+//       }
+//     });
+//   },
+//   {
+//     threshold: 0.3,
+//   }
+// );
+
+const item = document.querySelectorAll("scroll-animate");
+for (let i = 0; i < item.length; i++) {
+  oBserver.observe(item[i]);
+}
