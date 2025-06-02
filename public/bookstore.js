54bf29c175536c99701dec10b2be07af1749ea2b
nazisharifi2025 <banonazisharifi@gmail.com>
2025-06-02 21:52:56 +0430

    L

--- public/bookstore.js
+++ public/bookstore.js
@@ -95,7 +95,7 @@ const Productlist = {
     ),
     new Product("../public/img/hope.jpg", "The big hopes", "Charels Dicines"),
     new Product("../public/img/hall.jpg", "The hall of wolf", "Hellari mintal"),
-//     new Product("../public/img/fr.jpg", "Frankishtain", "Marry shelley"),
+    new Product("../public/img/fr.jpg", "Frankishtain", "Marry shelley"),
 //     new Product("../public/img/bad.jpg", "Badbadakbaz", "Khallid Hussaini"),
 //     new Product(
 //       "../public/img/bland.jpg",
